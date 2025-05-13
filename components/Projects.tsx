7f6da99d576d60b2d18375d113a821904efeb805
waliyinnura <waliyinnura@gmail.com>
2025-05-13 10:45:23 +0800

    chore: add section and animation on projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,12 +2,17 @@
 import React from "react";
 import { HoverEffect } from "./ui/HoverEffect";
 import { projects } from "@/data";
+import AnimatedSection from "./AnimatedSection";
 // import { PinContainer } from "./ui/3dPin";
 
 export function Projects() {
   return (
-    <div className="max-w-7xl mx-auto px-4">
-      <HoverEffect items={projects} />
-    </div>
+    <AnimatedSection>
+      <section id="projects">
+        <div className="max-w-7xl mx-auto px-4">
+          <HoverEffect items={projects} />
+        </div>
+      </section>
+    </AnimatedSection>
   );
 }
