dc7cf2cd29e5f3f87472cf9a8ed3a4f45476d33d
Wells <weligitlab@gmail.com>
2025-05-22 00:37:46 +0800

    chore: adjust performance

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,21 +5,21 @@ import MagicButton from "./ui/MagicButton";
 import Image from "next/image";
 
 const Footer = () => {
-  const year = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-[url('/footer-grid.svg')] bg-cover bg-center pt-20 pb-5 px-5">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl md:text-5xl font-bold text-center text-neutral-700 dark:text-neutral-400">
-          Ready to take <span className="text-blue-300">your</span> digital
-          presence to the next level?
+          Improve your <span className="text-blue-300">digital</span> presence
+          with me
         </h1>
         <p className="mt-5 text-center text-neutral-700 dark:text-neutral-400">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+          Let&apos;s discuss how I can help you achieve your goals.
         </p>
-        <a href="mailto:waliyinnura@gmail.com">
+        <a href="mailto:waliyinnura@gmail.com" className="mt-5">
           <MagicButton
-            title="Let's get in touch"
+            title="Get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
@@ -27,19 +27,19 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 justify-between items-center">
         <p className="text-sm md:text-base md:font-normal font-light">
-          Copyright &copy; {year} Well
+          Copyright {currentYear} Well
         </p>
         <div className="flex items-center gap-3">
-          {socialMedia.map((info) => (
+          {socialMedia.map((media) => (
             <a
-              key={info.id}
-              href={info.url}
+              key={media.id}
+              href={media.url}
               target="_blank"
               rel="noopener noreferrer"
               className="w-10 h-10 bg-black-300 rounded-lg flex justify-center items-center"
             >
               <Image
-                src={info.img}
+                src={media.img}
                 alt="icons"
                 width={20}
                 height={20}
