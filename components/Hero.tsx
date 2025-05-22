d5f11f32cd1c34d11d64e502771c71988d99587a
waliyinnura <waliyinnura@gmail.com>
2025-05-22 16:25:21 +0800

    chore: adjust for better performance

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,11 +12,11 @@ const Hero = () => (
         <h2 className="max-w-80 text-center text-[0.6rem] md:text-xs uppercase tracking-widest text-neutral-700 dark:text-neutral-400 pb-2">
           Well Portfolio
         </h2>
-        <h1 className="text-3xl md:text-6xl lg:text-7xl font-bold text-center text-neutral-700 dark:text-neutral-400 ">
+        <h1 className="text-2xl md:text-6xl lg:text-7xl font-bold text-center text-neutral-700 dark:text-neutral-400 ">
           Transforming Your Idea Into{" "}
           <span className="text-blue-300">Reality</span>
         </h1>
-        <span className="my-4 font-normal text-xs md:text-base text-neutral-700 dark:text-neutral-400 max-w-lg text-center mx-auto">
+        <span className="my-4 font-normal text-[0.7rem] md:text-base text-neutral-700 dark:text-neutral-400 max-w-lg text-center mx-auto">
           Hi! I&apos;m Well, a <FlipWords words={wordsHero} />
           Developer based in Indonesia.
         </span>
